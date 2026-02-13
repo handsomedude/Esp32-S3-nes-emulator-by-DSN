e2bfd4288136ff6804514ca29e1daa2c9c8039da
Dsn Industries <45259488+derdacavga@users.noreply.github.com>
2026-02-13 23:01:15 +0300

    Update noftypes.h

--- src/noftypes.h
+++ src/noftypes.h
@@ -6,13 +6,9 @@
 #define NOFRENDO_DEBUG
 #define NOFRENDO_MEM_DEBUG
 // #define NOFRENDO_VRAM_DEBUG
-// #define NOFRENDO_LOG_TO_FILE
-/* For the ESP32, it costs too much memory to render to a separate buffer and blit that to the main buffer.
-   Instead, the code has been modified to directly grab the primary buffer from the video subsystem and render
-   there, saving us about 64K of memory. */
+// #define NOFRENDO_LOG_TO_FILE 
 // #define NOFRENDO_DOUBLE_FRAMEBUFFER
-
-/* Define this if running on little-endian (x86) systems */
+ 
 #define HOST_LITTLE_ENDIAN
 
 #ifdef __GNUC__
@@ -21,12 +17,11 @@
 #elif defined(WIN32)
 #define INLINE static __inline
 #define ZERO_LENGTH 0
-#else /* crapintosh? */
+#else  
 #define INLINE static
 #define ZERO_LENGTH 1
 #endif
-
-/* quell stupid compiler warnings */
+ 
 #define UNUSED(x) ((x) = (x))
 
 typedef signed char int8;
@@ -44,11 +39,11 @@ typedef unsigned int uint32;
 #define ASSERT(expr) nofrendo_log_assert((int)(expr), __LINE__, __FILE__, NULL)
 #define ASSERT_MSG(msg) nofrendo_log_assert(false, __LINE__, __FILE__, (msg))
 
-#else /* !NOFRENDO_DEBUG */
+#else  
 
 #define ASSERT(expr)
 #define ASSERT_MSG(msg)
 
-#endif /* !NOFRENDO_DEBUG */
+#endif  
 
 #endif
