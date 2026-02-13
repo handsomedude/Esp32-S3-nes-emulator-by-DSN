3b7187c7fac427add0591a7c89196d3ee2c2b8c1
Dsn Industries <45259488+derdacavga@users.noreply.github.com>
2026-02-13 21:56:56 +0300

    Update nes_ppu.h

--- src/nes_ppu.h
+++ src/nes_ppu.h
@@ -2,8 +2,7 @@
 #define _NES_PPU_H_
 
 #include <bitmap.h>
-
-/* PPU register defines */
+ 
 #define  PPU_CTRL0            0x2000
 #define  PPU_CTRL1            0x2001
 #define  PPU_STAT             0x2002
@@ -16,47 +15,39 @@
 #define  PPU_OAMDMA           0x4014
 #define  PPU_JOY0             0x4016
 #define  PPU_JOY1             0x4017
-
-/* $2000 */
+ 
 #define  PPU_CTRL0F_NMI       0x80
 #define  PPU_CTRL0F_OBJ16     0x20
 #define  PPU_CTRL0F_BGADDR    0x10
 #define  PPU_CTRL0F_OBJADDR   0x08
 #define  PPU_CTRL0F_ADDRINC   0x04
 #define  PPU_CTRL0F_NAMETAB   0x03
-
-/* $2001 */
+ 
 #define  PPU_CTRL1F_OBJON     0x10
 #define  PPU_CTRL1F_BGON      0x08
 #define  PPU_CTRL1F_OBJMASK   0x04
 #define  PPU_CTRL1F_BGMASK    0x02
-
-/* $2002 */
+ 
 #define  PPU_STATF_VBLANK     0x80
 #define  PPU_STATF_STRIKE     0x40
 #define  PPU_STATF_MAXSPRITE  0x20
-
-/* Sprite attribute byte bitmasks */
+ 
 #define  OAMF_VFLIP           0x80
 #define  OAMF_HFLIP           0x40
 #define  OAMF_BEHIND          0x20
-
-/* Maximum number of sprites per horizontal scanline */
+ 
 #define  PPU_MAXSPRITE        8
-
-/* some mappers do *dumb* things */
+ 
 typedef void (*ppulatchfunc_t)(uint32 address, uint8 value);
 typedef void (*ppuvromswitch_t)(uint8 value);
 
 typedef struct ppu_s
-{
-   /* big nasty memory chunks */
+{ 
    uint8 nametab[0x1000];
    uint8 oam[256];
    uint8 palette[32];
    uint8 *page[16];
-
-   /* hardware registers */
+ 
    uint8 ctrl0, ctrl1, stat, oam_addr;
    uint32 vaddr, vaddr_latch;
    int tile_xofs, flipflop;
@@ -74,12 +65,10 @@ typedef struct ppu_s
 
    bool strikeflag;
    uint32 strike_cycle;
-
-   /* callbacks for naughty mappers */
+ 
    ppulatchfunc_t latchfunc;
    ppuvromswitch_t vromswitch;
-
-   /* copy of our current palette */
+ 
    rgb_t curpal[256];
 
    bool vram_accessible;
@@ -88,25 +77,20 @@ typedef struct ppu_s
    bool drawsprites;
 } ppu_t;
 
-
-/* TODO: should use this pointers */
+ 
 extern void ppu_setlatchfunc(ppulatchfunc_t func);
 extern void ppu_setvromswitch(ppuvromswitch_t func);
 
 extern void ppu_getcontext(ppu_t *dest_ppu);
 extern void ppu_setcontext(ppu_t *src_ppu);
-
-/* Mirroring */
-/* TODO: this is only used bloody once */
+ 
 extern void ppu_mirrorhipages(void);
 
 extern void ppu_mirror(int nt1, int nt2, int nt3, int nt4);
 
 extern void ppu_setpage(int size, int page_num, uint8 *location);
 extern uint8 *ppu_getpage(int page);
-
-
-/* control */
+ 
 extern void ppu_reset(int reset_type);
 extern bool ppu_enabled(void);
 extern void ppu_scanline(bitmap_t *bmp, int scanline, bool draw_flag);
@@ -115,18 +99,15 @@ extern void ppu_checknmi();
 
 extern ppu_t *ppu_create(void);
 extern void ppu_destroy(ppu_t **ppu);
-
-/* IO */
+ 
 extern uint8 ppu_read(uint32 address);
 extern void ppu_write(uint32 address, uint8 value);
 extern uint8 ppu_readhigh(uint32 address);
 extern void ppu_writehigh(uint32 address, uint8 value);
-
-/* rendering */
+ 
 extern void ppu_setpal(ppu_t *src_ppu, rgb_t *pal);
 extern void ppu_setdefaultpal(ppu_t *src_ppu);
-
-/* bleh */
+ 
 extern void ppu_dumppattern(bitmap_t *bmp, int table_num, int x_loc, int y_loc, int col);
 extern void ppu_dumpoam(bitmap_t *bmp, int x_loc, int y_loc);
 extern void ppu_displaysprites(bool display);
