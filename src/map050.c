bb54600e59c255b07828cabbeb1b0f21e6f244e7
Dsn Industries <45259488+derdacavga@users.noreply.github.com>
2026-02-13 18:37:36 +0300

    Refactor IRQ and initialization functions in map050.c

--- src/map050.c
+++ src/map050.c
@@ -9,90 +9,56 @@ static struct
   bool enabled;
   uint32 counter;
 } irq;
-
-/********************************/
-/* Mapper #50 IRQ reset routine */
-/********************************/
+ 
 static void map50_irq_reset(void)
-{
-  /* Turn off IRQs */
+{ 
   irq.enabled = false;
-  irq.counter = 0x0000;
-
-  /* Done */
+  irq.counter = 0x0000; 
   return;
 }
-
-/**************************************************************/
-/* Mapper #50: 3rd discovered variation of SMB2j cart bootleg */
-/**************************************************************/
+ 
 static void map50_init(void)
-{
-  /* Set the hardwired pages */
+{ 
   mmc_bankrom(8, 0x6000, 0x0F);
   mmc_bankrom(8, 0x8000, 0x08);
   mmc_bankrom(8, 0xA000, 0x09);
-  mmc_bankrom(8, 0xE000, 0x0B);
-
-  /* Reset the IRQ counter */
+  mmc_bankrom(8, 0xE000, 0x0B); 
   map50_irq_reset();
-
-  /* Done */
+ 
   return;
 }
-
-/****************************************/
-/* Mapper #50 callback for IRQ handling */
-/****************************************/
+ 
 static void map50_hblank(int vblank)
-{
-  /* Counter is M2 based so it doesn't matter whether */
-  /* the PPU is in its VBlank period or not           */
-  UNUSED(vblank);
-
-  /* Increment the counter if it is enabled and check for strike */
+{ 
+  UNUSED(vblank); 
   if (irq.enabled)
-  {
-    /* Is there a constant for cycles per scanline? */
-    /* If so, someone ought to substitute it here   */
-    irq.counter = irq.counter + 114;
-
-    /* IRQ line is hooked to Q12 of the counter */
+  { 
+    irq.counter = irq.counter + 114; 
     if (irq.counter & 0x1000)
-    {
-      /* Trigger the IRQ */
-      nes_irq();
-
-      /* Reset the counter */
+    { 
+      nes_irq(); 
       map50_irq_reset();
     }
   }
 }
-
-/******************************************/
-/* Mapper #50 write handler ($4000-$5FFF) */
-/******************************************/
+  
 static void map50_write(uint32 address, uint8 value)
 {
-  uint8 selectable_bank;
-
-  /* For address to be decoded, A5 must be high and A6 low */
+  uint8 selectable_bank; 
   if ((address & 0x60) != 0x20)
     return;
 
   /* A8 low  = $C000-$DFFF page selection */
   /* A8 high = IRQ timer toggle */
   if (address & 0x100)
-  {
-    /* IRQ settings */
+  { 
     if (value & 0x01)
       irq.enabled = true;
     else
       map50_irq_reset();
   }
   else
-  {
-    /* Stupid data line swapping */
+  { 
     selectable_bank = 0x00;
     if (value & 0x08)
       selectable_bank |= 0x08;
@@ -103,33 +69,19 @@ static void map50_write(uint32 address, uint8 value)
     if (value & 0x01)
       selectable_bank |= 0x04;
     mmc_bankrom(8, 0xC000, selectable_bank);
-  }
-
-  /* Done */
+  } 
   return;
 }
-
-/****************************************************/
-/* Shove extra mapper information into a SNSS block */
-/****************************************************/
+ 
 static void map50_setstate(SnssMapperBlock *state)
-{
-  /* TODO: Store SNSS information */
-  UNUSED(state);
-
-  /* Done */
+{ 
+  UNUSED(state); 
   return;
 }
-
-/*****************************************************/
-/* Pull extra mapper information out of a SNSS block */
-/*****************************************************/
+ 
 static void map50_getstate(SnssMapperBlock *state)
-{
-  /* TODO: Retrieve SNSS information */
-  UNUSED(state);
-
-  /* Done */
+{ 
+  UNUSED(state); 
   return;
 }
 
