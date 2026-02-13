946c29e77ce7b72c15cb5811f09d678d88987270
Dsn Industries <45259488+derdacavga@users.noreply.github.com>
2026-02-13 18:35:51 +0300

    Update map046.c

--- src/map046.c
+++ src/map046.c
@@ -8,43 +8,26 @@ static uint8 prg_low_bank;
 static uint8 chr_low_bank;
 static uint8 prg_high_bank;
 static uint8 chr_high_bank;
-
-/*************************************************/
-/* Set banks from the combined register values   */
-/*************************************************/
+ 
 static void map46_set_banks(void)
-{
-  /* Set the PRG and CHR pages */
+{ 
   mmc_bankrom(32, 0x8000, (prg_high_bank << 1) | (prg_low_bank));
   mmc_bankvrom(8, 0x0000, (chr_high_bank << 3) | (chr_low_bank));
-
-  /* Done */
+ 
   return;
 }
-
-/*********************************************************/
-/* Mapper #46: Pelican Game Station (aka Rumble Station) */
-/*********************************************************/
+ 
 static void map46_init(void)
-{
-  /* High bank switch register is set to zero on reset */
+{ 
   prg_high_bank = 0x00;
   chr_high_bank = 0x00;
   map46_set_banks();
-
-  /* Done */
+ 
   return;
 }
-
-/******************************************/
-/* Mapper #46 write handler ($6000-$FFFF) */
-/******************************************/
+ 
 static void map46_write(uint32 address, uint8 value)
-{
-  /* $8000-$FFFF: D6-D4 = lower three bits of CHR bank */
-  /*              D0    = low bit of PRG bank          */
-  /* $6000-$7FFF: D7-D4 = high four bits of CHR bank   */
-  /*              D3-D0 = high four bits of PRG bank   */
+{ 
   if (address & 0x8000)
   {
     prg_low_bank = value & 0x01;
@@ -56,33 +39,19 @@ static void map46_write(uint32 address, uint8 value)
     prg_high_bank = value & 0x0F;
     chr_high_bank = (value >> 4) & 0x0F;
     map46_set_banks();
-  }
-
-  /* Done */
+  } 
   return;
 }
-
-/****************************************************/
-/* Shove extra mapper information into a SNSS block */
-/****************************************************/
+ 
 static void map46_setstate(SnssMapperBlock *state)
-{
-  /* TODO: Store SNSS information */
-  UNUSED(state);
-
-  /* Done */
+{ 
+  UNUSED(state); 
   return;
 }
-
-/*****************************************************/
-/* Pull extra mapper information out of a SNSS block */
-/*****************************************************/
+ 
 static void map46_getstate(SnssMapperBlock *state)
-{
-  /* TODO: Retrieve SNSS information */
-  UNUSED(state);
-
-  /* Done */
+{ 
+  UNUSED(state); 
   return;
 }
 
