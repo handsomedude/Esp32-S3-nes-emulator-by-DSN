37d76f48c9cc474439ffc7824ec3ee1fe94c37e8
Dsn Industries <45259488+derdacavga@users.noreply.github.com>
2026-02-13 17:51:55 +0300

    Refactor map41 functions for clarity
    Refactor functions to remove unnecessary comments and improve readability.

--- src/map041.c
+++ src/map041.c
@@ -8,79 +8,59 @@ static uint8 register_low;
 static uint8 register_high;
  
 static void map41_set_chr(void)
-{
-  /* Set the CHR bank from the appropriate register bits */
+{ 
   mmc_bankvrom(8, 0x0000, ((register_low >> 1) & 0x0C) | (register_high));
-
-  /* Done */
+ 
   return;
 }
  
 static void map41_init(void)
-{
-  /* Both registers set to zero at power on */
-  /* TODO: Registers should also be cleared on a soft reset */
+{  
   register_low = 0x00;
   register_high = 0x00;
   mmc_bankrom(32, 0x8000, 0x00);
   map41_set_chr();
-
-  /* Done */
+ 
   return;
 }
  
 static void map41_low_write(uint32 address, uint8 value)
-{
-  /* Within this range the value written is irrelevant */
+{ 
   UNUSED(value);
-
-  /* $6000-$67FF: A5    = mirroring (1=horizontal, 0=vertical)      */
-  /*              A4-A3 = high two bits of 8K CHR bank              */
-  /*              A2    = register 1 enable (0=disabled, 1=enabled) */
-  /*              A2-A0 = 32K PRG bank                              */
+ 
   register_low = (uint8)(address & 0x3F);
   mmc_bankrom(32, 0x8000, register_low & 0x07);
   map41_set_chr();
   if (register_low & 0x20)
-    ppu_mirror(0, 0, 1, 1); /* horizontal */
+    ppu_mirror(0, 0, 1, 1);  
   else
-    ppu_mirror(0, 1, 0, 1); /* vertical */
-
-  /* Done */
+    ppu_mirror(0, 1, 0, 1);  
   return;
 }
  
 static void map41_high_write(uint32 address, uint8 value)
-{
-  /* Address doesn't matter within this range */
+{ 
   UNUSED(address);
-
-  /* $8000-$FFFF: D1-D0 = low two bits of 8K CHR bank */
+ 
   if (register_low & 0x04)
   {
     register_high = value & 0x03;
     map41_set_chr();
   }
-
-  /* Done */
+ 
   return;
 }
  
 static void map41_setstate(SnssMapperBlock *state)
-{
-  /* TODO: Store SNSS information */
+{ 
   UNUSED(state);
-
-  /* Done */
+ 
   return;
 }
  
 static void map41_getstate(SnssMapperBlock *state)
-{
-  /* TODO: Retrieve SNSS information */
-  UNUSED(state);
-
-  /* Done */
+{ 
+  UNUSED(state); 
   return;
 }
 
